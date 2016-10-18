10917c78bf03a5b76f245804bab436899053155d
mkrajnak <krajnakmatto@gmail.com>
2016-10-19 00:27:21 +0200

    time fields are bigger, values are translating e.g. minutes to hours when incrementing

--- prefs.js
+++ prefs.js
@@ -9,6 +9,7 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Extension = ExtensionUtils.getCurrentExtension();
 const Convenience = Extension.imports.convenience;
 
+const Pango = imports.gi.Pango;
 let settings;
 
 function init() {
@@ -59,37 +60,59 @@ const AutomaticShutdownTimerPrefs = new GObject.Class({
 
         //3rd row
         let hours = new Gtk.SpinButton({ orientation: Gtk.Orientation.VERTICAL});
-        let maxTimerValueDefault = settings.get_int('max-timer-value');
         hours.set_increments(1, 1);
-        hours.set_range(1, 60);
-        hours.connect('value-changed', Lang.bind(this, function(button){
-            let s = button.get_value_as_int();
-            settings.set_int('max-timer-value', s);
+        hours.modify_font(Pango.font_description_from_string('30'))
+        hours.set_range(0, 60);
+        hours.connect('value-changed', Lang.bind(this, function(){
         }));
         hours.set_value(maxTimerValueDefault);
         this.attach(hours, 2, 4, 1, 1);
         //this.add(hours);
 
         let minutes = new Gtk.SpinButton({ orientation: Gtk.Orientation.VERTICAL});
-        let maxTimerValueDefault = settings.get_int('max-timer-value');
-        minutes.set_increments(2, 2);
-        minutes.set_range(1, 60);
-        minutes.connect('value-changed', Lang.bind(this, function(button){
-            let s = button.get_value_as_int();
-            settings.set_int('max-timer-value', s);
+        minutes.set_increments(1, 1);
+        minutes.modify_font(Pango.font_description_from_string('30'))
+        minutes.set_range(0, 59);
+        minutes.set_value(0);
+
+        let tmp_min = minutes.get_value_as_int()
+        minutes.connect('value-changed', Lang.bind(this, function(){
+          let time = minutes.get_value_as_int()
+          if ( time === 60) {
+            if (tmp_min < time) {
+              minutes.set_value(0)
+              let val = hours.get_value_as_int() + 1
+              hours.set_value(val)
+            }
+          }
+          tmp_time = time;
         }));
-        minutes.set_value(maxTimerValueDefault);
         this.attach_next_to(minutes, hours, Gtk.PositionType.RIGHT, 1, 1);
 
+        // init seconds
         let seconds = new Gtk.SpinButton({ orientation: Gtk.Orientation.VERTICAL});
-        let maxTimerValueDefault = settings.get_int('max-timer-value');
+        seconds.modify_font(Pango.font_description_from_string('30'))
         seconds.set_increments(1, 1);
-        seconds.set_range(1, 60);
-        seconds.connect('value-changed', Lang.bind(this, function(button){
-            let s = button.get_value_as_int();
-            settings.set_int('max-timer-value', s);
+        seconds.set_range(0, 60);
+        // handle change
+        let tmp_secs = seconds.get_value_as_int()
+        seconds.connect('value-changed', Lang.bind(this, function(){
+          let cs_time = seconds.get_value_as_int()
+          if ( cs_time === 60) {
+            if (tmp_secs < cs_time) {
+              seconds.set_value(0)
+              let val = minutes.get_value_as_int() + 1
+              if (val === 60) {
+                  hours.set_value(hours.get_value_as_int() + 1)
+                  val = 0
+              }
+              minutes.set_value(val)
+            }
+          }
+          tmp_secs = cs_time;
+
         }));
-        seconds.set_value(maxTimerValueDefault);
+        seconds.set_value(0);
         this.attach_next_to(seconds, minutes, Gtk.PositionType.RIGHT, 1, 1);
 
         this.attach(new Gtk.HSeparator(), 0, 5, 6, 1);
